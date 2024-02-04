7c9b03095ec6ce9bbfdc3f76da6ade52bc0cce0d
losman333 <losman333@users.noreply.github.com>
2024-02-04 14:20:13 -0800

    abstractions

--- Higher Order Function.js
+++ Higher Order Function.js
@@ -1,8 +1,19 @@
+/** large program is costly
+ * size involves complexity
+ * introduces bugs(mistakes)
+*/
 // Abstraction //
-
+/**
+ * what do abstractions do?
+ * 
+ * hide details and give us the ability to talk about problems at a higher level(abstract
+ * 
+ * useful to notice when you are working a low level on abstraction
+ */
 
 
 //Abstraction Repetition//
+
 
 //Higher Order Functions //
 
@@ -48,8 +59,47 @@ Arrays provide useful higher-order methods
 // Excercises //
 
 
-/* flatten */
 
-//Your own loop
 
-/* dominant writing direction */
+/* flatten
+use reduce method in combination with concact method to flatten an
+array of arrays into a single array that has all the elements
+of the orginal arrays
+*/
+
+
+/*Your own loop
+write a high-order function loop that provides a for loop statement. 
+Takes a 
+value a 
+test function 
+update function
+a body function
+
+Each iteration first runs the test function on current loop stops 
+if returns false
+calls the body function giving it the current value
+finally calls the update function to create a new value and starts from the begining
+Use a regular loop to do the actual looping when defining the function
+
+*/
+
+
+/** Everything 
+ * every mehod is opposite some method
+ * returns truewhen the given function returens 
+ * true for every element in the array. 
+ * some method a version of the || operator that acts on arrays
+ * every is like the && operator on arrays 
+ *
+ * Implement every as a function that takes an array and a predicate
+ * function as parameters
+ * 
+ * write two version one using a loop and one using the some method
+*/
+
+/* dominant writing direction 
+write a function that computes the dominant writing direction in a string of text
+use characterScript
+and countBy functions
+*/
