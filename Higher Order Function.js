c48197fc9096d77cbd40e3ee8551c78e50378b0c
losman333 <losman333@users.noreply.github.com>
2024-02-09 16:17:41 -0800

    dd

--- Higher Order Function.js
+++ Higher Order Function.js
@@ -194,7 +194,18 @@ function reduce(array, combine, start) {
     }
     return current;
 }
-console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));  ad
+console.log(reduce([1, 2, 3, 4], (a, b) => a + b, 0));  
+
+console.log([1, 2, 3, 4].reduce((a, b) => a + b));
+
+// to reduce twice to find script with most characters//
+
+function characterCount(script) {
+    return script.ranges.reduce((count, [from, to]) => {
+        return count + (to - from);
+
+    }, 0);
+}
 
 
 // Composability //
