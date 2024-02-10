fcc2c3aed627ae158f39ed8453f381e820cc7bdb
losman333 <losman333@hotmail.com>
2024-02-09 22:06:49 -0800

    neww

---
+++ 6. Secret life Objects.js
@@ -0,0 +1,38 @@
+
+// Encapsulation
+
+// Methods
+
+// Prototypes
+
+// Classes
+
+// Class Notation
+
+// Overriding derived properties
+
+// Maps
+
+// Polymorphism
+
+// Symbols
+xa
+// Iterator Interface
+adasdf
+// Getters, Settings, Statics
+
+// Inheritance
+
+// Instanceof Operator
+
+// Summary
+
+// Exercies
+
+// A Vector Type
+
+// Groups
+
+// Iterable Groups
+
+// Borrowing a method
