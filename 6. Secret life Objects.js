7b5bfbbcd0c3d439ca94a84e3abaf6d27f5a522e
losman333 <losman333@users.noreply.github.com>
2024-02-10 22:25:15 -0800

    send

--- 6. Secret life Objects.js
+++ 6. Secret life Objects.js
@@ -5,10 +5,59 @@
  * into smaller pieces
  * make each responsible for managing its not
  * 
+ * knowledge about the piece of program
+ * works can be kept local to that piece
+ * 
+ * when local details change only code
+ * directly around needs to be updated
+ * 
+ * different pieces of program interact with 
+ * each other through interfaces ( sets of functions
+ * or bindings that provide useful functionality 
+ * at more abstract level)
+ * 
+ * program pieces are modeled using objects
+ * interface consists of a specific set of methods
+ * and properties
+ * 
+ * Properties of that are part of interface are called public
+ * code that should not be touched are called private
+ * 
+ * JS does not distinguish public and private props
+ * and prevent outside code from accessing private ones
+ * 
+ * underscore used at start of property names indicate
+ * those are private
+ * 
+ * Encapsulation seperates interface from implementation
  */
 
-// Methods f
+// Methods 
 
+/**
+ * methods are properties hold function values
+ */
+
+let rabbit = {};
+rabbit.speak = function(line) {
+    console.log(`The rabbit says'${}'`);
+};
+
+rabbit.speak("I'm alive.");
+
+/**
+ * method needs to do something with object it was called
+ * 
+ * when func is called as a method--looked up and called
+ * as in object.method()--binding called this in its body
+ * points at the object that was called on
+ */
+
+function speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+}
+let whiteRabbit = {type: "white", speak};
+let hungryRabbit = {type: "hungry", speak};
 // Prototypes
 
 // Classes
