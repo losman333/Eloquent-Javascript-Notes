11ac7bd224d71e713fcd17f1946fa58f0e981ae5
losman333 <losman333@users.noreply.github.com>
2024-02-21 17:37:37 +0000

    village state

--- 7. Project a Robot.js
+++ 7. Project a Robot.js
@@ -31,16 +31,22 @@ class VillageState {
             let parcels = this.parcel.map(p => {
                 if (p.place != this.place) return p;
                 return {place: desitination, address: p.address};
-
+            // call to filter does the delivering //
             }).filter(p => p.place != p.address);
             
     }
 
 
 
-// call to filter does the delivering //
 
 // move method gives new village state leaves old one intact //
+
+let first = new VillageState(
+    "Post Office",
+    [{place: "Post Office", address: "Alice's House"}]
+    );
+    let next = first.move("Alice's House");
+    console.log(next.place); // → Alice's House console.log(next.parcels); // → [] console.log(first.place); // → Post Office
 
 // move causes parcel to be delivered reflected in next state
 
@@ -56,8 +62,62 @@ class VillageState {
 
 // freezing requires more computer work can confuse someone //
 
+// a given object shouldn't be messed with 
+
+let object = Object.freeze({value: 5});
+object.value = 10;
+console.log(object.value);
+// 5
+
 // Simulation
 
+/**
+ * Robot is a function that takes a VillageState object
+ * returns the name of the place
+ * robot returns direction it wants to move in
+ * a memory value that will be given back next time
+ * it is called
+ */
+
+function runRobot (state, robot, memory) {
+    for (let turn = 0;; turn++) {
+        if (state.parcels.length == 0)
+    }   console.log(`Done in ${turn} turns`);
+        break;
+    let action = robot(state, memory);
+    state = state.move(action.direction);
+    memory = action.memory;
+    console.log(`Moved to ${action.direction}`);
+    }
+}
+
+/**
+ * Math.randon() returns a number between zero and one--but
+ *  always below one //
+ * Multiply by length of arrray then applying Math.floor 
+ * gives random index for array
+ * 
+ * javacript funtion can be called with extra arguments without 
+ * ill effects omits memory propert in it return object
+ * 
+ * Create new state with some parcels
+ * write static method by adding a property to the constructor
+ */ 
+
+VillageState.random = function() {
+    let
+    for () {
+        let address
+        place
+        do {
+            place
+            while
+        }
+        parcels
+    }
+    return
+}
+// do loop keeps picking new places when equal to the address//
 // The Mail Trucks Route
 
 // Pathfinding
