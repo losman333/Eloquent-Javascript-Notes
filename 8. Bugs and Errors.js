d116b5c30cff51fd69b6f726bd7807b5b88f768b
losman333 <losman333@users.noreply.github.com>
2024-02-23 00:21:45 -0800

    easy to test

--- 8. Bugs and Errors.js
+++ 8. Bugs and Errors.js
@@ -2,9 +2,99 @@
 
 // Strict mode
 
+/**
+ * doesn't work when binding already exists
+ * this binding holds the value undefined
+ * that are not called as methods
+ * 
+ * when making call outside of strict this refers to 
+ * global scope object, object with properties of 
+ * global bindings
+ * 
+ * following code calls constructor function without
+ * new keyword this will not refer to a newly constructed object
+ */
+
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand"); //oops
+console.log(name);
+
+//Ferdinand//
+"use strict";
+function Person(name) { this.name = name; }
+let ferdinand = Person("Ferdinand"); // forgot new
+// → TypeError: Cannot set property 'name' of undefined
+
+/**
+ * 1. disallows giving function same name removrs 
+ * 2. removes problamatic features like with statements
+ * 
+ * //strict used to trouble shoot bugs 
+ */
+
 // types
 
+// what is the purpose of types
+
+/**
+ * provide useful framework for talking about programs.
+ * reduces the confusion around which vales
+ * go in and out of function. 
+ */
+
+// why is it used
+
+// how is it used
+
+/**
+ * you can give randomPick a type like ([T]) 
+ * to stand in for any type
+ * using a type variable
+ * 
+ * TypeScript add types to JS in order to check
+ * 
+ * more objects code interacts with harder it is
+ * to set up context which to test it. 
+ * self-contained persistent values tend to be
+ * easy to test
+ */
+
+// example
+
 // testing
+
+// what is it//
+/**
+ * process of writing tests another program
+ * use "test suites" language in the form 
+ * of functions and methods
+ * outputs info when test fails
+ * called test runners
+ * 
+ */
+
+// whats the purpose of using test//
+
+//example//
+/**
+ * test take form of labled programs to verify
+ * aspect of code
+ * 
+ * test for the toUpperCase method
+ */
+
+function test(label, body) {
+    if (!body()) console.log(`Failed: ${label}`);
+}
+test("convert Lating text to uppercase", () => {
+    return "hello".toUpperCase() == "HELLO";
+});
+
+test("convert Greek text to uppercase", () => { return "Χαίρετε".toUpperCase() == "ΧΑΊΡΕΤΕ";
+;)}
+
+test("don't convert case-less characters", () => {
+;"مرحبا" == )(toUpperCase."مرحبا" return ;)}
 
 // debugging
 
@@ -19,3 +109,4 @@
 // assertions
 
 // summary
+
