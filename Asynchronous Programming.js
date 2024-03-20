df48762da5ad10eb068a23cdc348c47327c5c160
losman333 <losman333@users.noreply.github.com>
2024-03-20 15:49:26 -0700

    promise

--- Asynchronous Programming.js
+++ Asynchronous Programming.js
@@ -69,3 +69,86 @@ setTimeout(() => console.log("Tick"), 500);
 
 import {bigOak} from "./crow-tech";
 
+bigOak.readStorage("food caches", caches => {
+    let firstCache = caches[0];
+    bigOak.readStorage(firstCache, info => {
+        console.log(info);
+    });
+});
+
+/**
+ * request response pairs use types
+ * determines how response is handled
+ * 
+ * interface exported by "./crow-tech" module
+ * provides callback-based functions for communication
+ * nests have a send method that sends of request
+ * expects the name of the target nest, type of request, 
+ * and content of request as first three arguements
+ * expects a function to call when a response comes in 
+ * as its fourth last argument 
+ */
+
+bigOak.send("Cow Pasture", "note", "Let's caw loudly at 7pm", 
+            () => console.log("Note delivered."));
+
+/**
+ * to be able to receive message define a request type
+ * to handle requests 
+ */
+
+import {defineRequestType} from "./crow-tech";
+
+defineRequestType("note", (nest, content, source, done) => {
+    console.log('${nest.name} received note: ${content}');
+    done();
+});
+
+/**
+ * 
+ * note request sends note to given nest. 
+ * console.log will verify if note has arrived
+ * fourth argument done is callback function 
+ * must call when done with request
+ * cant perform async action if handlers return
+ * value is response value
+ * 
+ * function doing async work returns before work 
+ * is done
+ * 
+ * callback function to signal a response is
+ * available
+ * 
+ * any function that calls an async function must
+ * be asynchronus, using a callback or similar
+ * mechanism to deliver result. 
+ */
+
+// Promises
+
+/**
+ * abstract concepts can be represented by values
+ * return object that represents future event
+ * 
+ * promise async action that may complete a some pintna
+ * and produce a value. 
+ * 
+ * call promise using Promise.resolve
+ * func ensures value given is wrapped in a promise                            
+ */
+
+let fifteen = Promise.resolve(15);
+
+let fifteen = Promise.resolve(15);
+use then metod.      
+fifteen.then(value => console.log(`Got ${value}`));
+
+/** to results of promise use then method
+ * registers a callback function to be called when promise
+ * resolves and produces value
+ * add multiple clallbacks to single promise will be 
+ * called even if you add them after promise has resolved
+ * 
+ * then method returns another promise resolves value
+ * that handler function returns 
+ */
