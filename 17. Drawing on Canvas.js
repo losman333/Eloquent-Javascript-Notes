d877317e066ca3ccb29832193a1315d9a565ea42
losman333 <losman333@users.noreply.github.com>
2024-04-25 15:19:57 -0700

    svg

---
+++ 17. Drawing on Canvas.js
@@ -0,0 +1,54 @@
+
+/**
+ * Several ways to display graphics in browser
+ * 
+ * use styles to position and color regular Dom Elements
+ * 
+ * using transform style
+ * 
+ * Scalable Vector Graphics SVG (Dom-based)
+ * document-markup dialect focuses on shapes 
+ * rather than text
+ * 
+ * you can embed an SVG doc directly in an HTML doc
+ * or include with an <img>
+ * 
+ * Canvas single DOM element encapsluates a picture
+ * provides programming interface for drawing
+ * shapes onto space taken by node
+ * 
+ * svg shapes preserved to moved or resize any time
+ * canvas converts shapes to pixels when drawn
+ * does not remember what pixels represent
+ * 
+ * only way to move shape on canvas is to clear
+ * canvas or part of ocanvas around shape
+ * redraw it wih shape in new position
+ */
+//SVG
+
+// The canvas element
+
+// Lines and Surfaces
+
+// Paths
+
+// Curves
+
+// Drawing a pie chart
+
+// Text
+
+// Images
+
+// Transformation
+
+// Storing and clearning transformations
+
+// Back to the game
+
+// Choosing a graphics interface
+
+// Summary
+
+// Exercies
