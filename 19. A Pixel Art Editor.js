0c0f4c24c004023dd85726336260bd64e9ec9a8b
losman333 <losman333@users.noreply.github.com>
2024-05-30 15:24:41 -0700

    saving and loading

--- 19. A Pixel Art Editor.js
+++ 19. A Pixel Art Editor.js
@@ -493,6 +493,124 @@ class ColorSelect {
 
 // drawing tools
 
+/**
+ * implement tools to control
+ * functionality of mouse 
+ * or touch events on the canvas
+ * 
+ * draw tool updates
+ * picture to version which
+ * pointed-at pixel is given
+ * currently selected color
+ */
+
+function draw(pos, state, dispatch) {
+    function drawPixel({x, y}, state) {
+        let drawn = {x, y, color: state.color};
+        dispatch({picture: state.picture.draw([drawn])});
+    }
+    drawPixel(pos, state);
+    return drawPixel;
+}
+
+/**
+ * function calls drawPixel function
+ * returns so that it is called again
+ * for newly touched pixels when
+ * the user drags or swiptes over the
+ * the picture
+ * 
+ * use rectangle tool draws a rectangle
+ * between the point where you
+ * start dragging and the point that
+ * you drag to.
+ */
+
+function rectangle(starte, state, dispatch) {
+    function drawRectangle(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        for (let y = yStart; y <= yEnd; y++) {
+            for (let x = xStart; x <= xEnd; x++) {
+                drawn.push({x, y, color: state.color});
+            }
+        }
+        dispatch({picture: state.picture.draw(drawn)});
+    }
+    drawRectangle(start);
+    return drawRectangle;
+}
+
+/**
+ * when dragging, rectangle is redrawn
+ * on the picture from the original state
+ * to make rectangle large and smaller
+ * again while creating it
+ * 
+ * immutable picture objects are usefull
+ * 
+ * implementing flood fill, more involved
+ * 
+ * a tool that fills pixel under pointer
+ * and all adjacent pixels that have same
+ * color
+ * 
+ * flood fill uses pathfinding code 
+ * searches through a grid to find all
+ * connected pixels
+ */
+
+const around = [{dx: -1, dy: 0}, {dx: 1, dy: 0},
+                {dx: 0, dy: -1}, {dx: 0, dy: 1}];
+
+function fill({x, y}, state, dispatch) {
+    let targetColor = state.picture.pixel(x, y);
+    let drawn = [{x, y, color: state.color}];
+    for (let done = 0; done < drawn.length; done++) {
+        for (let {dx, dy} of around) {
+            let x = drawn[done].x +dx, y = drawn[done].y + dy;
+            if (x >= 0 && x < state.picture.width &&
+                y >= 0 && y < state.picture.height &&
+                state.picture.pixel(x, y) == targetColor &&
+                !drawn.some(p => p.x == x && p.y == y) {
+                    drawn.push({x, y, color: state.color});
+                }
+            )
+        }
+        dispatch({picture: state.picture.draw(drawn)});
+    }
+}
+
+/**
+ * arrawy of drawn pixels doubles 
+ * as functions work list
+ * 
+ * for each pixel reached we have to see
+ * whether any adjacent pixels 
+ * have the same color and have't been
+ * painted over
+ * 
+ * loop counter lags behind length of drawn
+ * array as new pixels are added
+ * 
+ * Any pixels ahead of it still need to be explored
+ * 
+ * when it catches up with length no unexplored pixels
+ * remain an function is done
+ * 
+ * final tool is a color picker which allows 
+ * you to point at a color in the picture
+ * to use it as the current drawing color
+ * 
+ */
+
+function pic(pos, state, dispatch) {
+    dispatch({color: state.picture.pixel(pos.x, pos.y)});
+}
+
 // saving and loading
 
 // undo history
