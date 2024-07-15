268b911762256133fbb1e8ccd947fc267fb17286
losman333 <losman333@users.noreply.github.com>
2024-07-15 15:40:41 -0700

    page 600

--- 21. Skill-Sharing Website.js
+++ 21. Skill-Sharing Website.js
@@ -801,6 +801,46 @@ function renderTalkForm(dispatch) {
 }
 
 // Polling
+async function pollTalks(update) {
+    let tag = undefined;
+    for (;;) {
+        let response;
+        try {
+            response await fetchOK("/talks", {
+                headers: tag && {"If-None-Match"; tag, 
+                                 "Prefer": "wait=90"
+                }
+            }); 
+            } catch(e) {
+                console.log("Request failed: " + e);
+                await new Promise(resolve =>> setTimeout(resolve, 500));
+                continue;
+        }
+        if (response.status == 304) continue;
+        tag = response.headers.get("ETag");
+        update(await response.json());
+    }
+}
+
+/**
+ * async function
+ * for looping and waiting
+ * 
+ * headers included make
+ * it a long polling
+ * request
+ * 
+ * when request fails 
+ * function waits a moment
+ * then tries again
+ * 
+ * if network goes 
+ * away for a while
+ * application can recover
+ * and continue updating
+ */
+
+
 // the excercises
 
 // Exercises
