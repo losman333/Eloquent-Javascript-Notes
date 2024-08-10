a2159094a452a4fb832b57ffe5cbe64a8b1e4215
losman333 <losman333@users.noreply.github.com>
2024-08-10 15:30:27 -0700

    binding

---
+++ 2. Program Structure.js
@@ -0,0 +1,49 @@
+/**
+ * an expression in js
+ * 
+ * a fragment of code that produces
+ * value is called an expression
+ * 
+ * use expression to communicate complex
+ * computations 
+ * 
+ * values have to be framed in a 
+ * larger stucture to be useful
+ * 
+ * expressions can contain other expressions
+ * 
+ * javascript statement corresponds to ful setencee
+ * 
+ * a programm is a list of statements n
+ * 
+ * expressions have semi-colon 
+ */
+
+1;
+
+!false
+
+/**
+ * a statement changes something or 
+ * affects statements that come af
+ * 
+ * every statement needs a semi-colon
+ * 
+ * 
+ * how does javascript hold values
+ * to catch and hold values
+ * JS provides binding or variable
+ * 
+ * let caught = 5 * 5
+ * 
+ * let defines a binding
+ * followed by name of binding (caught) 
+ * 
+ * after binding is defined
+ * its name can be used as an expression
+ * the value of an expression is that
+ * value the binding currently holds
+ * 
+ * let ten = 10;
+ * console.log(ten * ten);
+ */
